95f7f9988321aaad4a1c74d9f0c916e7ed321b33
baotran <brightsmile959674@gmail.com>
2023-02-13 21:14:17 +0700

    example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:inf_flutter_logger/inf_flutter_logger.dart';
+import 'package:inf_flutter_logger/logger.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,8 +32,8 @@ class _MyAppState extends State<MyApp> {
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await _infFlutterLoggerPlugin.getPlatformVersion() ?? 'Unknown platform version';
+      platformVersion = await _infFlutterLoggerPlugin.getPlatformVersion() ??
+          'Unknown platform version';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
@@ -55,7 +56,17 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Column(
+            children: [
+              Text('Running on: $_platformVersion\n'),
+              TextButton(
+                  onPressed: () {
+                    Logger.info('Demo info');
+                    Logger.error('Demo error');
+                  },
+                  child: Text('Click and view console'))
+            ],
+          ),
         ),
       ),
     );
