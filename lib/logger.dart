25ae6296e868d49fecde2088b8bdb8a0caaead78
baotran@inftech.vn <brightsmile959674@gmail.com>
2023-08-27 20:26:50 +0700

    unit test

--- lib/logger.dart
+++ lib/logger.dart
@@ -5,31 +5,31 @@ import 'package:flutter/foundation.dart';
 class Logger {
   static void error(String message, {String tag = "❌"}) {
     if (kDebugMode) {
-      log(message);
+      log('$tag $message');
     }
   }
 
   static void info(String message, {String tag = "ℹ️"}) {
     if (kDebugMode) {
-      log(message);
+      log('$tag $message');
     }
   }
 
   static void warning(String message, {String tag = "🟡"}) {
     if (kDebugMode) {
-      log(message);
+      log('$tag $message');
     }
   }
 
   static void req(String message, {String tag = "⬆️"}) {
     if (kDebugMode) {
-      log(message);
+      log('$tag $message');
     }
   }
 
   static void res(String message, {String tag = "⬇️"}) {
     if (kDebugMode) {
-      log(message);
+      log('$tag $message');
     }
   }
 }
--- lib/test/log_test.dart
+++ lib/test/log_test.dart
@@ -4,6 +4,10 @@ import 'package:test/test.dart';
 void main() {
   test('info', () {
     Logger.info('bao test info');
+    Logger.warning('bao test warning');
+    Logger.error('bao test error');
+    Logger.req('bao test req');
+    Logger.res('bao test res');
     expect(true, true);
   });
 }
